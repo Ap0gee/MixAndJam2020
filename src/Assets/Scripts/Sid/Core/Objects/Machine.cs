dbdc772924d2015b5e3f2ac26d9e4a4112209504
Sjshovan <SJSHOVAN@GMAIL.COM>
2020-11-14 09:39:21 -0500

    working on interaction ui

---
+++ src/Assets/Scripts/Sid/Core/Objects/Machine.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace GameJam
+{
+    [Serializable]
+    enum MachineTypes
+    {
+        Fabricator,
+        Mill,
+        Extruder,
+        Press,
+        Forge,
+        Mining,
+        PickNPlace
+    }
+
+    public class Machine : MonoBehaviour
+    {
+        [SerializeField]
+        private MachineTypes m_type;
+
+        MachineTypes Type
+        {
+            get { return m_type; }
+        }
+    }
+}
