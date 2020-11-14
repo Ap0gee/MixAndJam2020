5519e2d7ce9c91edc5b752042d244af96207cb54
Sjshovan <SJSHOVAN@GMAIL.COM>
2020-11-14 03:46:19 -0500

    add house script

---
+++ src/Assets/Scripts/Sid/Core/Objects/House.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+namespace GameJam
+{
+    public class House : MonoBehaviour
+    {
+        private UnityEvent<float> healthChanged;
+
+        [SerializeField]
+        private Image m_healthBar;
+
+        [SerializeField]
+        private float m_maxHealth = 100f;
+
+        private float m_health;
+
+        public float Health
+        {
+            get { return m_health; }
+            set { if (value <= 0) { m_health = 0; } else if (m_health >= m_maxHealth) { m_health =  m_maxHealth; } else { m_health = value; } OnHealthChanged(m_health); }
+        }
+
+        public bool Destroyed
+        {
+            get { return m_health == 0; }
+        }
+
+        private void Awake()
+        {
+            m_health = m_maxHealth;
+            OnHealthChanged(m_health);
+            healthChanged.AddListener(OnHealthChanged);
+        }
+
+        private void OnHealthChanged(float newVal)
+        {
+            m_healthBar.fillAmount = 1 / m_maxHealth * m_health;
+        }
+    }
+}
